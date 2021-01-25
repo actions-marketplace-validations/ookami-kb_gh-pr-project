bce002c6ffca057262d01e5cf57d0ff3653fe73c
ookami-kb <ookami.kb@gmail.com>
2021-01-25 17:51:50 +0100

    :bug: Append existing projects

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import {context, GitHub} from '@actions/github'
+import {PullRequest} from '@octokit/graphql-schema'
 
 async function run(): Promise<void> {
   try {
@@ -12,20 +13,64 @@ async function run(): Promise<void> {
       return
     }
 
+    const projectIds = Array.from([
+      projectId,
+      ...(await getExistingProjectIds(gitHub, pr.node_id))
+    ])
+
     const mutation = `
-      mutation AddProject($prId: ID!, $projectId: ID!) {
+      mutation AddProject($prId: ID!, $projectIds: [ID!]!) {
         updatePullRequest(
-          input: {pullRequestId: $prId, projectIds: [$projectId]}
+          input: {pullRequestId: $prId, projectIds: $projectIds}
         ) {
           clientMutationId
         }
       }
     `
 
-    await gitHub.graphql(mutation, {prId: pr.node_id, projectId})
+    await gitHub.graphql(mutation, {prId: pr.node_id, projectIds})
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+async function getExistingProjectIds(
+  gitHub: GitHub,
+  prId: string
+): Promise<Iterable<string>> {
+  const pullRequestQuery = `
+      query GetProjects($prId: ID!) {
+        nodes(ids: [$prId]) {
+          ... on PullRequest {
+            id
+            projectCards {
+              nodes {
+                project {
+                  id
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  const result = await gitHub.graphql<{nodes: PullRequest[]}>(
+    pullRequestQuery,
+    {prId}
+  )
+
+  const projects = new Set<string>()
+
+  for (const node of result.nodes) {
+    const cards = node.projectCards.nodes
+    if (cards == null) break
+    for (const card of cards) {
+      const existingProjectId = card?.project.id
+      if (existingProjectId != null) projects.add(existingProjectId)
+    }
+  }
+
+  return projects
 }
 
 run()
